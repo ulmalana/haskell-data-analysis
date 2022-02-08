19d462388db6e56da3932f86da2259ad2bb218c8
ulmalana <muhamaul@gmail.com>
2022-02-08 16:20:01 +0700

    [HDA] updated chapter 2

--- chapter-2/Chapter02.hs
+++ chapter-2/Chapter02.hs
@@ -7,6 +7,9 @@ import Text.CSV
 avg :: (Real a, Fractional b) => [a] -> b
 avg xs = realToFrac (sum xs) / fromIntegral (length xs)
 
+readColumn :: [String] -> [Double]
+readColumn = map read
+
 getColumnInCSV :: CSV -> String -> Either String Integer
 getColumnInCSV csv columnName =
     case lookupResponse of 
@@ -16,12 +19,26 @@ getColumnInCSV csv columnName =
 
 applyToColumnInCSV :: ([String] -> b) -> CSV -> String -> Either String b
 applyToColumnInCSV func csv column = either
-                    (Left)
-                    (Right . func . elements)
-                columnIndex
-                where
-                    columnIndex = getColumnInCSV csv column
-                    nfieldsInFile = length $ head csv
-                    records = tail $
-                        filter (\record -> nfieldsInFile == length record) csv
-                    elements ci = map (\record -> genericIndex record ci) records
+                                        (Left)
+                                        (Right . func . elements)
+                                        columnIndex
+                                      where
+                                        columnIndex = getColumnInCSV csv column
+                                        nfieldsInFile = length $ head csv
+                                        records = tail $ filter (\record -> nfieldsInFile == length record) csv
+                                        elements ci = map (\record -> genericIndex record ci) records
+
+-- Opens a CSV file and applies a function to a column
+-- Returns Either Error Message or the function result
+applyToColumnInCSVFile :: ([String] -> b) -> FilePath -> String -> IO (Either String b)
+applyToColumnInCSVFile func inFileName column = do
+    -- Open and read the CSV file
+    input <- readFile inFileName
+    let records = parseCSV inFileName input
+    -- Check to make sure this is a good csv file
+    return $ either
+        (handleCSVError)
+        (\csv -> applyToColumnInCSV func csv column)
+        records
+    where
+        handleCSVError csv = Left "This does not appear to be a CSV file."
