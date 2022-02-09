2d7af4aadeaccb2c9e634a2c2b51e9622e53c3c6
ulmalana <muhamaul@gmail.com>
2022-02-09 16:19:38 +0700

    [HDA] finished chapter 2

--- chapter-2/Chapter02.hs
+++ chapter-2/Chapter02.hs
@@ -3,6 +3,8 @@ module Chapter02 where
 import Data.List
 import Data.Either
 import Text.CSV
+import Database.HDBC
+import Database.HDBC.Sqlite3
 
 avg :: (Real a, Fractional b) => [a] -> b
 avg xs = realToFrac (sum xs) / fromIntegral (length xs)
@@ -42,3 +44,58 @@ applyToColumnInCSVFile func inFileName column = do
         records
     where
         handleCSVError csv = Left "This does not appear to be a CSV file."
+
+
+-- converts CSV to SQL DB
+-- return "Sucsessful" if success
+convertCSVToSQL :: String -> FilePath -> [String] -> CSV -> IO ()
+convertCSVToSQL tableName outFile fields records = 
+    -- make sure # column == # fields
+    if nfieldsInFile == nfieldsInFields then do 
+        conn <- connectSqlite3 outFile 
+
+        -- create new table
+        run conn createStatement []
+
+        -- load CSV content to table
+        stmt <- prepare conn insertStatement
+        executeMany stmt (tail (filter (\record -> 
+            nfieldsInFile == length record) 
+            sqlRecords))
+
+        -- commit changes and close connection
+        commit conn
+        disconnect conn
+
+        putStrLn "Successful"
+    else 
+        putStrLn "The number of input fields does not match the csv."
+    where
+        nfieldsInFile = length $ head records
+        nfieldsInFields = length fields
+        createStatement = "CREATE TABLE " ++
+                            tableName ++ 
+                            " (" ++ (intercalate ", " fields) ++")"
+        insertStatement = "INSERT INTO " ++
+                            tableName ++ " VALUES (" ++
+                            (intercalate ", " (replicate nfieldsInFields "?")) ++ ")"
+        sqlRecords = map (\record -> map (\element -> toSql element) record) records 
+
+
+-- convert CSV file to SQL DB
+-- test with: 
+-- ghci> convertCSVFileToSQL "all_week.csv" "earthquakes.sql" "oneWeek" ["time
+--        TEXT", "latitude REAL", "longitude REAL", "depth REAL", "mag REAL",
+--        "magType TEXT", "nst INTEGER", "gap REAL", "dmin REAL", "rms REAL", "net
+--         REAL", "id TEXT", "updated TEXT", "place TEXT", "type TEXT" horizontalError REAL", 
+--         "depthError REAL", "magError REAL", "magNst INTEGER", "status TEXT", "locationSource TEXT", "magSource TEXT"]
+
+convertCSVFileToSQL :: String -> String -> String -> [String] -> IO ()
+convertCSVFileToSQL inFile outFile tableName fields = do 
+    input <- readFile inFile 
+    let records = parseCSV inFile input
+
+    either handleCSVError convertTool records
+  where
+    convertTool = convertCSVToSQL tableName outFile fields
+    handleCSVError csv = putStrLn "This does not appear to be a CSV file."
