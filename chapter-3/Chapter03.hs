6f8cd7cf60ef4ae43de47b1dca8a4bd11c463d68
ulmalana <muhamaul@gmail.com>
2022-02-14 16:26:55 +0700

    [HDA] updated chapter 3

--- chapter-3/Chapter03.hs
+++ chapter-3/Chapter03.hs
@@ -4,6 +4,7 @@ import Text.CSV
 import Data.List
 import Text.Regex.Posix ((=~))
 import System.Environment (getArgs)
+import Chapter02
 
 -- check the length of field in each record
 countFieldsInEachRecord :: CSV -> [Integer]
@@ -20,3 +21,44 @@ lineNumberWithIncorrectCount (fields:csv) = filter
     nfields = genericLength fields 
     count = countFieldsInEachRecord csv
     lineNoCountPairs = zip [1..] count
+
+identifyMatchingFields :: (String -> Bool)
+                        -> [String]
+                        -> [String]
+                        -> Integer
+                        -> [(String, String, String)]
+identifyMatchingFields myStringCmpFunc record headings idColumnIndex = 
+   filter 
+      (\(_, _, field) -> myStringCmpFunc field)
+      keyvalue
+    where
+      nfields = length headings
+      keyvalue = zip3 (replicate nfields (genericIndex record idColumnIndex))
+                 headings
+                 record
+
+identifyInCSV :: (String -> Bool) -> CSV -> String ->
+                    Either String [(String, String, String)]
+identifyInCSV myFieldFunc csv idColumn =
+    either
+        (Left)
+        (\ci -> Right $ concatMap
+            (\record ->
+                identifyMatchingFields myFieldFunc record (head csv) ci
+                )
+                (tail csv))
+        columnIndex
+    where
+        headings = head csv 
+        columnIndex = getColumnInCSV csv idColumn
+
+identifyInCSVFile :: (String -> Bool) -> String -> String -> IO (Either String [(String, String, String)])
+identifyInCSVFile myStringCmpFunc inFileName idColumn = do
+    records <- parseCSVFromFile inFileName
+    return $ either
+                (\err ->
+                 Left "This does not appear to be a CSV file")
+                (\csv ->
+                    identifyInCSV myStringCmpFunc (init csv) idColumn
+                )
+                records
