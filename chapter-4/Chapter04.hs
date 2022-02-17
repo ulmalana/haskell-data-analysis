2944c7f7606914d8f157e25cb6640fa8ffb0b0e8
ulmalana <muhamaul@gmail.com>
2022-02-17 14:26:01 +0700

    [HDA] finished chapter 4

--- chapter-4/Chapter04.hs
+++ chapter-4/Chapter04.hs
@@ -16,15 +16,41 @@ readStringColumn :: [[SqlValue]] -> Integer -> [String]
 readStringColumn sqlResult index = map (\ row -> fromSql $ genericIndex row index :: String) sqlResult
 
 queryDatabase :: FilePath -> String -> IO [[SqlValue]]
-queryDatabase databaseFile sqlQuery = do
-    conn <- connectSqlite3 databaseFile
+queryDatabase dbFile sqlQuery = do
+    conn <- connectSqlite3 dbFile
     result <- quickQuery' conn sqlQuery []
     disconnect conn
     return  result
 
 pullStockClosingPrices :: String -> String -> IO [(Double, Double)]
-pullStockClosingPrices databaseFile database = do
-    sqlResult <- queryDatabase databaseFile ("SELECT rowid, adjclose FROM " ++ database)
+pullStockClosingPrices dbFile db = do
+    sqlResult <- queryDatabase dbFile ("SELECT rowid, adjclose FROM " ++ db)
+    
     -- this line is different from the books (without reverse)
     -- since the data is already in chronological order
     return $ zip (readDoubleColumn sqlResult 0) (readDoubleColumn sqlResult 1)
+
+percentChange :: Double -> Double -> Double
+percentChange value first = 100.0 * (value - first) / first
+
+applyPercentChangeToData :: [(Double, Double)] -> [(Double, Double)]
+applyPercentChangeToData dataset = zip indices scaledData
+    where 
+        (_, first) = last dataset
+        indices = reverse [1.0..(genericLength dataset)]
+        scaledData = map (\(_, value) -> percentChange value first) dataset
+
+movingAverage :: [Double] -> Integer -> [Double]
+movingAverage values window 
+  | window >= genericLength values = [ avg values ]
+  | otherwise = avg (genericTake window values) : (movingAverage (tail values) window)
+
+applyMovingAverageToData :: [(Double, Double)] -> Integer -> [(Double, Double)]
+applyMovingAverageToData dataset window = zip [fromIntegral window..] $
+                                            movingAverage (map snd (reverse dataset))
+                                            window
+
+pullLatitudeLongitude :: String -> String -> IO [(Double, Double)]
+pullLatitudeLongitude dbFile db = do 
+    sqlResult <- queryDatabase dbFile ("SELECT latitude, longitude FROM " ++ db)
+    return $ zip (readDoubleColumn sqlResult 1) (readDoubleColumn sqlResult 0)
