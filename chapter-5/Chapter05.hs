7e296a92de319cb154ce6fc6eb8e97e3228ad63e
ulmalana <muhamaul@gmail.com>
2022-02-21 15:45:40 +0700

    [HDA] finished chapter 5

--- chapter-5/Chapter05.hs
+++ chapter-5/Chapter05.hs
@@ -1,8 +1,16 @@
 module Chapter05 where 
 
+import Data.List
 import Math.Combinatorics.Exact.Binomial
 import Chapter02
 import Chapter04
 
 probabilityMassFunction :: Integral a => a -> a -> Double -> Double
 probabilityMassFunction k n p = (fromIntegral (n `choose` k)) * (p^k) * ((1-p)^(n-k))
+
+standardDeviation :: [Double] -> Double
+standardDeviation values = 
+    (sqrt . sum $ map (\x -> (x-mu)*(x-mu)) values) / sqrt_nml
+  where
+    mu = avg values
+    sqrt_nml = sqrt $ (genericLength values - 1)
