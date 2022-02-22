b603ef01ffa468fc5d363211f7b7a1bb54a09e50
ulmalana <muhamaul@gmail.com>
2022-02-22 15:04:47 +0700

    [HDA] started chapter 6 and separated the code from the data

---
+++ src/Chapter06.hs
@@ -0,0 +1,23 @@
+module Chapter06 where
+
+import Data.List
+import Graphics.EasyPlot
+import Chapter02
+import Chapter04
+import Chapter05
+
+covariance :: [Double] -> [Double] -> Double
+covariance x y = avg $ zipWith (\xi yi -> (xi - xavg) * (yi - yavg)) x y 
+  where 
+    xavg = avg x 
+    yavg = avg y
+
+pearsonR :: [Double] -> [Double] -> Double
+pearsonR x y = r 
+  where 
+    xstdev = standardDeviation x 
+    ystdev = standardDeviation y 
+    r = covariance x y / (xstdev * ystdev)
+
+pearsonRSqrd :: [Double] -> [Double] -> Double
+pearsonRSqrd x y = pearsonR x y ^ 2
